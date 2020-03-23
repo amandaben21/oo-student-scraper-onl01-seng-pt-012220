83d3b169d53541a4e9efeecbb79c4fc9bdb19a06
Amanda Contreras <amandaben21@aol.com>
2020-03-23 19:32:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    doc.css(student-card).text
+    doc.css("student-card").each do |student|
 
   end
 
