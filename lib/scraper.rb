9a9b9145e524254b983a5e7557fd045959f1a596
Amanda Contreras <amandaben21@aol.com>
2020-03-23 20:01:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
       index_page = {
         :name => student.css("h4").text,
         :location => student.css("p").text,
-        :profile_url => student.css("a").attribute
+        :profile_url => student.css("a").attribute("href").value
       }
 
   end
